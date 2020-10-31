8d754b7bc09c23e6dbedaeb940276e03065a65a7
nguyentrunghieu2001 <73011079+nguyentrunghieu2001@users.noreply.github.com>
2020-10-31 15:23:26 +0700

    Add files via upload

---
+++ Bai2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Units
+    {
+        public static double Tinh(int x)
+        {
+            return Math.Pow(x, (float)1 / 3);
+
+        }
+        public static int C(int k, int n)
+        {
+            if (k == 0 || k == n) return 1;
+            if (k == 1) return n;
+            return C(k - 1, n - 1) + C(k, n - 1);
+        }
+        public static int giaithua(int n)
+        {
+            if (n == 0) return 1;
+            else return n * giaithua(n - 1);
+        }
+        public static int A(int k, int n)
+        {
+            if (n == k) return giaithua(n);
+            if (k == 0) return 1;
+            return n * A(k - 1, n - 1);
+        }
+    }
+    class App2
+    {
+        static void Main3()
+        {
+            Units t = new Units();
+            Console.WriteLine("Gia tri bieu thuc={0}", Units.Tinh(8));
+            Console.WriteLine("Gia tri bieu thuc C(6,9)={0}", Units.C(6, 9));
+            Console.WriteLine("Gia tri bieu thuc A={0}", Units.A(2, 6));
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Bai3.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thuchanh3
+{
+    class Time
+    {
+        private int gio, phut, giay;
+        public Time()
+        {
+            gio = 0;
+            phut = 0;
+            giay = 0;
+        }
+        public Time(int gio, int phut, int giay)
+        {
+            this.gio = gio;
+            this.phut = phut;
+            this.giay = giay;
+        }
+        public int Gio
+        {
+            get
+            {
+                return gio;
+            }
+            set
+            {
+                gio = value;
+            }
+        }
+        public int Phut
+        {
+            get
+            {
+                return phut;
+            }
+            set
+            {
+                phut = value;
+            }
+        }
+        public int Giay
+        {
+            get
+            {
+                return giay;
+            }
+            set
+            {
+                giay = value;
+            }
+        }
+        public Time normalize()
+        {
+            this.phut += giay / 60;
+            this.giay = giay % 60;
+            this.gio += phut % 60;
+            this.phut = phut % 60;
+            this.gio = gio % 24;
+            return this;
+        }
+        public Time advance(int gio, int phut, int giay)
+        {
+            Time t = new Time();
+            t.gio = this.gio + gio;
+            t.phut = this.phut + phut;
+            t.giay = this.giay + giay;
+            t.phut += t.giay / 60;
+            t.giay = t.giay % 60;
+            t.gio += t.phut % 60;
+            t.phut = t.phut % 60;
+            t.gio = t.gio % 24;
+            return t;
+
+        }
+        public void print()
+        {
+            Console.WriteLine("Thoi gian:{0}:{1}:{2}", gio, phut, giay);
+        }
+
+    }
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Time t = new Time(23, 561, 89);
+            Time t2 = t.normalize();
+            t2.print();
+            Time t3 = t.advance(35, 2, 24);
+            t3.print();
+            Console.ReadKey();
+        }
+    }
+}
